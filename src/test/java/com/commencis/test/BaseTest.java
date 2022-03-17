da8b47ba674258b50516b11a98253c2c893d1a22
canoklay <fatihcan.oklay@vigo.ist>
2022-03-17 15:30:01 +0300

    update gitignore

--- src/test/java/com/commencis/test/BaseTest.java
+++ src/test/java/com/commencis/test/BaseTest.java
@@ -5,7 +5,6 @@ import com.commencis.utility.DriverFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.time.Duration;
